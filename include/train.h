c1f865238d7baee86eb3841dc3c5c839fadf8d45
munderr <mvyazov534@gmail.com>
2025-05-02 17:52:29 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -16,5 +16,6 @@ class Train {
   void addCar(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
   int getOpCount();         // вернуть число переходов (из вагона в вагон)
+  void resetOpCount();
 };
 #endif  // INCLUDE_TRAIN_H_
